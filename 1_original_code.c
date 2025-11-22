41c291bc1ec559b64b0f3d632ac5f1de4e302b64
0xMichaelRan <michaelran16@gmail.com>
2025-11-23 01:23:58 +0800

    original code

---
+++ 1_original_code.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int main(void) {
+    int num1, num2, op, result1, result2;
+    
+    srand((unsigned)time(NULL));
+    num1 = rand() % 10 / (RAND_MAX + 1);
+    num2 = rand() % 10 / (RAND_MAX + 1);
+    op = rand() % 3 / (RAND_MAX + 1);
+    
+    switch(op) {
+        case 0:
+            printf("%d+%d=", num1, num2);
+            scanf("%d", &result1);
+            if(num1 + num2 == result1)
+                printf("You are right!\n");
+            else
+                printf("You are wrong!\n");
+            break;
+            
+        case 1:
+            printf("%d-%d=", num1, num2);
+            scanf("%d", &result1);
+            if(num1 - num2 == result1)
+                printf("You are right!\n");
+            else
+                printf("You are wrong!\n");
+            break;
+            
+        case 2:
+            printf("%d*%d=", num1, num2);
+            scanf("%d", &result1);
+            if(num1 * num2 == result1)
+                printf("You are right!\n");
+            else
+                printf("You are wrong!\n");
+            break;
+            
+        case 3:
+            printf("%d%%d=", num1, num2);
+            scanf("%d", &result1);
+            printf("余数为 = ");
+            scanf("%d", &result2);
+            if((num1 / num2 == result1) && (num1 % num2 == result2))
+                printf("You are right!\n");
+            else
+                printf("You are wrong!\n");
+            break;
+    }
+    
+    return 0;
+}
